635ee0817ddcb393e34f51a096cdd7d0974ed311
Riley Swedberg <swedbergr@unk.edu>
2024-09-26 14:29:01 -0500

    Update and rename stopwatch.py to appointment_scheduler.py

---
+++ appointment_scheduler.py
@@ -0,0 +1,18 @@
+# Display purpose of program to user
+print("Hello, and welcome to Dr. Smith's office. In order to schedule your appointment, " + 
+     "please provide your preference and we will match it to our availability.")
+
+
+# TODO (Your code here)
+
+
+
+
+
+
+
+
+
+# Display appointment details
+print(f"You are scheduled for an appointment with Dr. Smith on {day_of_week} at " + 
+     f"{time_specific} in the {time_of_day}.")
