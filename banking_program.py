538c596d12bbe936407623cb38a5df1e748b6727
Riley Swedberg <swedbergr@unk.edu>
2024-10-10 18:16:51 -0500

    Update and rename guessing_game.py to banking_program.py

---
+++ banking_program.py
@@ -0,0 +1,15 @@
+# Set accumulators
+balance = 0
+withdraw_counts = 0
+deposit_counts = 0
+
+# TODO Start code here
+
+
+
+
+# Display results
+print("\tSummary\n---------------")
+print(f"Current Balance:\t ${balance:,.2f}.")
+print(f"Number of Withdraws: {withdraw_counts:>5}.")
+print(f"Number of Deposits:  {deposit_counts:>5}.")
--- guessing_game.py
+++
@@ -1,7 +0,0 @@
-# Import random library
-import random
-
-# Get random number for user to guess
-target = random.randrange(100)
-
-# TODO Enter your code here
