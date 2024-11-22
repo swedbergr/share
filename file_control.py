c56d89deaadc0de0661c310c3beb0a1e803e2bb8
Riley Swedberg <swedbergr@unk.edu>
2024-11-22 15:57:39 +0000

    Task Tracker Lab

---
+++ file_control.py
@@ -0,0 +1,9 @@
+# Create a function called load_tasks that reads tasks from a file
+# into a list and then returns the list.
+
+
+
+
+# Create a function called save_tasks that takes a list of tasks and 
+# writes them to a file for long non-volatile storage.
+
--- main.py
+++ main.py
@@ -1,3 +1,60 @@
 # Import library of functions
-import helper
+import file_control
 
+def main():
+  # Get tasks from file
+  tasks = load_tasks()
+
+  # Create loop for menu
+  while True:
+    print("---Task Tracker Menu---")
+    print("1. Display tasks",
+         "\n2. Add tasks",
+         "\n3. Mark task as complete",
+         "\n4. Remove Task",
+         "\n5. Save and exit")
+
+    # Get user choice
+    choice = input()
+
+    # Navigate user based on choice
+    if choice == "1":
+      display_tasks(tasks)
+    elif choice == "2":
+      add_tasks(tasks)
+    elif choice == "3":
+      complete(tasks)
+    elif choice == "4":
+      remove(tasks)
+    elif choice == "5":
+      save_tasks(tasks)
+      # Exit program
+      print("Thank you for using Task Tracker.")
+      break
+
+
+
+# Create a function called display_tasks that takes a list of taks and
+# displays every task in the list.
+
+
+
+
+
+# Create a function called add_tasks that takes a list of tasks, prompts
+# the user for another task, and then appends the new tasks to the 
+# end of the list.
+
+
+
+
+
+# Create a function called complete that takes a lists of tasks,
+# displays them to the user, and then lets the user choose one
+# to mark as complete. It will then update the status of the 
+# task in the list and return the updated list.
+
+
+
+if __name__ == "__main__":
+  main()
