893acdc6c6ab7f0b3bfc29ce9521022102aefaf0
Riley Swedberg <swedbergr@unk.edu>
2026-03-25 23:22:04 -0500

    2.5-2.6 Assignment

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# Write ALL code for the program in this file. You are welcome to copy and paste functions from previous assignments.
+def main():
+    '''
+    Main function that administrates receiving a 2x2 matrix
+    and creates the X-Lambda-X_inverse factorization.
+    '''
+    # WRITE YOUR CODE HERE
+    A = [[]] # Change the assignment for original A matrix
+    X = [[]] # Change the assignment for X matrix
+    Lambda = [[]] # Change the assignment for Lambda matrix
+    X_inverse = [[]] # Change the assignment for X_inverse matrix
+
+
+    # Ouput that MUST be kept
+    print(f"{A}={X}*{Lambda}*{X_inverse}")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
